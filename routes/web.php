05a85166d94272a2f3785c117108c46a0074e891
uhcardoso <werlendev@gmail.com>
2021-05-17 17:43:41 -0300

    adiciona grupo de rotas

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,65 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/login', function() {
+    return 'login';
+})->name('login');
+
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+Route::middleware([])->group(function() {
+    Route::prefix('/admin')->group(function() {
+        Route::namespace('Admin')->group(function() {
+            Route::name('admin.')->group(function() {
+                Route::get('/dashboard', 'TesteController@Teste')->name('dashboard');
+
+                Route::get('/financeiro', 'TesteController@Teste')->name('financeiro');
+
+                Route::get('/produtos', 'TesteController@Teste')->name('produtos');
+
+                Route::get('/', function() {
+                    return redirect()->route('admin.dashboard');
+                })->name('admin.home');
+            });
+        });
+    });
+});
 */
+
+Route::group([
+    'middleware' => [],
+    'prefix' => 'admin',
+    'namespace' => 'Admin'
+],function() {
+    Route::get('/dashboard', 'TesteController@Teste')->name('admin.dashboard');
+
+    Route::get('/financeiro', 'TesteController@Teste')->name('admin.financeiro');
+
+    Route::get('/produtos', 'TesteController@Teste')->name('admin.produtos');
+
+    Route::get('/', function() {
+        return redirect()->route('admin.dashboard');
+    })->name('admin.home');
+});
+
+Route::get('/redirect3', function() {
+    return redirect()->route('url.name');
+});
+
+Route::get('/nome-url', function() {
+    return 'ha ha ha';
+})->name('url.name');
+
+Route::view('/view', 'welcome');
+
+Route::redirect('/redirect1', '/redirect2');
+
+// Route::get('/redirect1', function() {
+//     return redirect('/redirect2');
+// });
+
+Route::get('/redirect2', function() {
+    return 'Redirect 02';
+});
 
 Route::get('/produtos/{idProduct?}', function($idProduct = '') {
     return "Produto: ${idProduct}";
